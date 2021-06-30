7c542523a607c85bed171b7f8db7bfb64f699974
Carlos Brazil <cabrazil@gmail.com>
2021-06-30 13:23:48 -0300

    redesign with nextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | gng510</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
